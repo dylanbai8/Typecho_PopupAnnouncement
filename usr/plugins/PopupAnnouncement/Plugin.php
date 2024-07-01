b11299a396d19df578debb0d850a24993d5d1ebc
白大哥 <dylanbai8@gmail.com>
2024-07-01 23:32:43 +0800

    Update Plugin.php

--- usr/plugins/PopupAnnouncement/Plugin.php
+++ usr/plugins/PopupAnnouncement/Plugin.php
@@ -2,10 +2,10 @@
 if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 
 /**
- * PopupAnnouncement 插件
+ * 活动倒计时，弹框公告。
  *
  * @package PopupAnnouncement
- * @version 2.0
+ * @version 3.0
  * @author chatgpt
  * @link https://github.com/dylanbai8
  */
@@ -23,14 +23,20 @@ class PopupAnnouncement_Plugin implements Typecho_Plugin_Interface
 
     public static function config(Typecho_Widget_Helper_Form $form)
     {
-        $announcement = new Typecho_Widget_Helper_Form_Element_Textarea('announcement', null, '', _t('公告内容'), _t('请输入公告内容，插入&lt;br&gt;换行'));
+        $announcement = new Typecho_Widget_Helper_Form_Element_Textarea('announcement', null, '清仓大甩卖！<br>清仓大甩卖！', _t('公告内容'), _t('请输入公告内容，插入&lt;br&gt;换行'));
         $form->addInput($announcement);
 
-        $countdownEndTime = new Typecho_Widget_Helper_Form_Element_Text('countdownEndTime', null, '', _t('倒计时结束时间'), _t('请输入倒计时结束时间，格式为 YYYY-MM-DD HH:MM:SS'));
+        $countdownEndTime = new Typecho_Widget_Helper_Form_Element_Text('countdownEndTime', null, '2050-10-01 01:01:01', _t('倒计时结束时间'), _t('请添加倒计时结束时间，例如 2050-10-01 01:01:01'));
         $form->addInput($countdownEndTime);
 
-        $stayDuration = new Typecho_Widget_Helper_Form_Element_Text('stayDuration', null, '3000', _t('停留时长 (毫秒)'), _t('请输入弹框在中央停留的时长，以毫秒为单位 (1000毫秒=1秒)'));
+        $stayDuration = new Typecho_Widget_Helper_Form_Element_Text('stayDuration', null, '3', _t('停留时长 (秒)'), _t('请输入弹框在中央停留的时长，以秒为单位'));
         $form->addInput($stayDuration);
+
+        $popupWidth = new Typecho_Widget_Helper_Form_Element_Text('popupWidth', null, '250px', _t('自定义弹框宽度'), _t('请输入弹框的宽度，例如 290px'));
+        $form->addInput($popupWidth);
+
+        $popupColor = new Typecho_Widget_Helper_Form_Element_Text('popupColor', null, '#d1d1d1d1', _t('自定义弹框颜色'), _t('请输入弹框的颜色，例如 #d1d1d1d1'));
+        $form->addInput($popupColor);
     }
 
     public static function personalConfig(Typecho_Widget_Helper_Form $form)
@@ -42,6 +48,11 @@ class PopupAnnouncement_Plugin implements Typecho_Plugin_Interface
         $announcement = Typecho_Widget::widget('Widget_Options')->plugin('PopupAnnouncement')->announcement;
         $countdownEndTime = Typecho_Widget::widget('Widget_Options')->plugin('PopupAnnouncement')->countdownEndTime;
         $stayDuration = Typecho_Widget::widget('Widget_Options')->plugin('PopupAnnouncement')->stayDuration;
+        $popupWidth = Typecho_Widget::widget('Widget_Options')->plugin('PopupAnnouncement')->popupWidth;
+        $popupColor = Typecho_Widget::widget('Widget_Options')->plugin('PopupAnnouncement')->popupColor;
+
+        // 去除公告内容中的换行符
+        $announcement = str_replace(array("\r", "\n", "'"), '', $announcement);
 
         echo '<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>';
         echo '<style>
@@ -64,8 +75,8 @@ class PopupAnnouncement_Plugin implements Typecho_Plugin_Interface
             top: 50%;
             left: 50%;
             transform: translate(-50%, -50%);
-            background: rgba(209, 209, 209, 0.9);
-            width: 230px;
+            background: ' . $popupColor . ';
+            width: ' . $popupWidth . ';
             padding: 20px;
             border: 1px solid #ccc;
             z-index: 9999;
@@ -84,7 +95,7 @@ class PopupAnnouncement_Plugin implements Typecho_Plugin_Interface
 $(document).ready(function() {
     var announcement = '$announcement';
     var countdownEndTime = new Date('$countdownEndTime').getTime();
-    var stayDuration = parseInt('$stayDuration');
+    var stayDuration = parseInt('$stayDuration') * 1000;
 
     var x = setInterval(function() {
         var now = new Date().getTime();
@@ -97,9 +108,9 @@ $(document).ready(function() {
 
         if (distance < 0) {
             clearInterval(x);
-            $('#popup-announcement-content').html('<p>活动已结束</p>');
+            $('#popup-announcement-content').html('<p>活动已结束！</p>');
         } else {
-            $('#popup-announcement-content').html('<p>' + announcement + '</p><p>距离结束: ' + days + '天 ' + hours + '小时 ' + minutes + '分钟 ' + seconds + '秒</p>');
+            $('#popup-announcement-content').html('<p>' + announcement + '</p><p>距离结束: ' + days + '天 ' + hours + '时 ' + minutes + '分 ' + seconds + '秒</p>');
         }
     }, 1000);
 
